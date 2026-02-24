a374cfe4beb6c5d5de47865f48967d3e3b16de15
launa9840 <launashrestha@gmail.com>
2026-02-24 20:24:48 +0545

    jjjj

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-slate-50 dark:bg-slate-900">
+    <div className="min-h-screen bg-slate-50 dark:bg-slate-800">
       <Navbar />
       <Dashboard />
     </div>
